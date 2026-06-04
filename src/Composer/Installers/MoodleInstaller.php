4e688520f2dd4baa911579b94bf86b66e9ad2bd5
Tasos <anastasios.koutoumanos@gmail.com>
2026-06-04 15:40:23 +0300

    added support for Moodle "smsgateway" plugins

--- src/Composer/Installers/MoodleInstaller.php
+++ src/Composer/Installers/MoodleInstaller.php
@@ -60,6 +60,7 @@ class MoodleInstaller extends BaseInstaller
         'report'             => 'report/{$name}/',
         'repository'         => 'repository/{$name}/',
         'scormreport'        => 'mod/scorm/report/{$name}/',
+        'smsgateway'         => 'sms/gateway/',
         'search'             => 'search/engine/{$name}/',
         'theme'              => 'theme/{$name}/',
         'tiny'               => 'lib/editor/tiny/plugins/{$name}/',
